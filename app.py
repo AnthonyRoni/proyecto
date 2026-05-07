472e9c363207908629dda593e0a7a049abc32358
AnthonyRoni <anthonycastillopatron@gmail.com>
2026-05-07 12:59:31 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, redirect
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import os
+
+app = Flask(__name__)
+
+# URI de Mongo Atlas
+MONGO_URI = os.environ.get("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+
+db = client["sistema_tareas"]
+coleccion = db["tareas"]
+
+@app.route('/')
+def index():
+
+    pendientes = list(
+        coleccion.find({"completada": False})
+    )
+
+    completadas = list(
+        coleccion.find({"completada": True})
+    )
+
+    return render_template(
+        'index.html',
+        pendientes=pendientes,
+        completadas=completadas
+    )
+
+@app.route('/agregar', methods=['POST'])
+def agregar():
+
+    nombre = request.form['tarea']
+
+    nueva_tarea = {
+        "nombre": nombre,
+        "completada": False
+    }
+
+    coleccion.insert_one(nueva_tarea)
+
+    return redirect('/')
+
+@app.route('/completar/<id>')
+def completar(id):
+
+    coleccion.update_one(
+        {"_id": ObjectId(id)},
+        {"$set": {"completada": True}}
+    )
+
+    return redirect('/')
+
+@app.route('/eliminar/<id>')
+def eliminar(id):
+
+    coleccion.delete_one(
+        {"_id": ObjectId(id)}
+    )
+
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
